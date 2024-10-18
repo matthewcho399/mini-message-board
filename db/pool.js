577a21b0439448b9e45686a6cf3edce6bdb5a271
Matthew <matthewcho399@gmail.com>
2024-10-18 16:30:31 -0700

    debug

--- db/pool.js
+++ db/pool.js
@@ -1,21 +1,21 @@
-// const { Pool } = require("pg");
-// require("dotenv").config();
+const { Pool } = require("pg");
+require("dotenv").config();
 
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-// });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 
-// pool.on("error", (err) => {
-//   console.error("Unexpected error on idle client", err);
-//   process.exit(-1);
-// });
+pool.on("error", (err) => {
+  console.error("Unexpected error on idle client", err);
+  process.exit(-1);
+});
 
-// pool.connect((err) => {
-//   if (err) {
-//     console.error("Failed to connect to PostgreSQL:", err);
-//   } else {
-//     console.log("Successfully connected to PostgreSQL");
-//   }
-// });
+pool.connect((err) => {
+  if (err) {
+    console.error("Failed to connect to PostgreSQL:", err);
+  } else {
+    console.log("Successfully connected to PostgreSQL");
+  }
+});
 
-// module.exports = pool;
+module.exports = pool;
