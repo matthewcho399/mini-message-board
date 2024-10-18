434e82a21f234e71bc33b1e72261eea71a3c6ed6
Matthew <matthewcho399@gmail.com>
2024-10-18 13:43:45 -0700

    prod db

--- db/pool.js
+++ db/pool.js
@@ -2,7 +2,12 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_PUBLIC_URL,
+  connectionString: process.env.DATABASE_URL,
+});
+
+pool.on("error", (err) => {
+  console.error("Unexpected error on idle client", err);
+  process.exit(-1);
 });
 
 pool.connect((err) => {
