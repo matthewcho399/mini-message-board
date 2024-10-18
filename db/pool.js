8e5720d54337b6bb9cee692a37c9e5f7111cc0ec
Matthew <matthewcho399@gmail.com>
2024-10-18 16:32:25 -0700

    debug

--- db/pool.js
+++ db/pool.js
@@ -5,11 +5,6 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
-pool.on("error", (err) => {
-  console.error("Unexpected error on idle client", err);
-  process.exit(-1);
-});
-
 pool.connect((err) => {
   if (err) {
     console.error("Failed to connect to PostgreSQL:", err);
