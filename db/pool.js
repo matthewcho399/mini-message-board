762e7b36ed3e3d210fb2fb056aeacd6f9ef44794
Matthew <matthewcho399@gmail.com>
2024-10-18 13:13:27 -0700

    prod db

--- db/pool.js
+++ db/pool.js
@@ -2,5 +2,5 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 module.exports = new Pool({
-  connectionString: process.env.PROD_DB_URL,
+  connectionString: process.env.DATABASE_URL,
 });
