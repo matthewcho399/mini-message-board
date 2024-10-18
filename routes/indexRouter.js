2847e21ecf038a45558861ee3b03a3e31cdf6da7
Matthew <matthewcho399@gmail.com>
2024-10-18 16:26:36 -0700

    debug

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,12 +1,52 @@
+// const { Router } = require("express");
+// const controller = require("../controllers/controller");
+// const indexRouter = Router();
+
+// indexRouter.get("/", controller.getAllMessages);
+
+// indexRouter.get("/new", controller.createMessageGet);
+// indexRouter.post("/new", controller.createMessagePost);
+
+// indexRouter.get("/message/:id", controller.getMessage);
+
+// module.exports = indexRouter;
+
 const { Router } = require("express");
-const controller = require("../controllers/controller");
+
 const indexRouter = Router();
 
-indexRouter.get("/", controller.getAllMessages);
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
 
-indexRouter.get("/new", controller.createMessageGet);
-indexRouter.post("/new", controller.createMessagePost);
+indexRouter.get("/", (req, res) =>
+  res.render("index", { title: "Mini Messageboard", messages: messages })
+);
 
-indexRouter.get("/message/:id", controller.getMessage);
+indexRouter.get("/new", (req, res) => res.render("form"));
+
+indexRouter.post("/new", (req, res) => {
+  const messageUser = req.body.name;
+  const messageText = req.body.message;
+  messages.push({
+    text: messageText,
+    user: messageUser,
+    added: new Date(),
+  });
+  res.redirect("/");
+});
+
+indexRouter.get("/message/:id", (req, res) => {
+  res.render("message", { message: messages[req.params.id] });
+});
 
 module.exports = indexRouter;
